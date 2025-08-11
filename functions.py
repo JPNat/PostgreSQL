d8cf545f4ab4920a8042f64b85c8bbce597dfd36
JPNat <jpnatividade2016@gmail.com>
2025-08-10 21:49:59 -0300

    Arquivos melhorados

---
+++ functions.py
@@ -0,0 +1,16 @@
+#Aqui será onde teremos as funcionalidades
+
+def create_User(name):
+
+    #query = "INSERT INTO usuario (id, nome, disciplina) VALUES (%s, %s, %s)"
+    #with conn.cursor() as cursor:
+        #cursor.execute(query, (id, name, email,))
+    #conn.commit()
+
+    print("\n Criado com sucesso!")
+
+
+def list_User(name):
+
+    print(f"\n {name}")
+    
--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
 import psycopg2
+import psycopg2.extras
+from functions import create_User, list_User
 
 hostname = 'bancodedadosufs.c6cw1k5vxwqq.us-east-1.rds.amazonaws.com'
 database = 'postgres'
 username = 'aluno'
 password = 'GNUrPQ1TSTcjlr779me2'
 port = 5432
-
+conn = None
+cur = None
 
 try:
 
@@ -19,14 +22,45 @@ try:
 
     )
 
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory= psycopg2.extras.DictCursor)
+
+
+    while True:
+
+        print("\nMenu:")
+        print("1 - Inserir Usuario")
+        print("2 - Listar Usuario")
+        print("3 - Sair")
+
+        opcao = int(input("\nDigite a opção: ")) # Input único e claro
+
+        
+        if opcao == 1:
+                
+            name = input("Qual o nome do Usuário: ")
+            create_User(name)
+            #conn.commit()
+            
+        if opcao == 2:
+
+            name = input("Qual o nome que deseja buscar: ")
+            list_User(name)
+            #conn.commit()
+            
+        if opcao == 3:
+                
+            print("Hasta la vista! \n")
+            break
 
     
-    cur.close()
-    conn.close()
 
+    conn.commit()
 except Exception as error:
     print(error)
 
-
-
+finally:
+    if cur is not None:
+        cur.close()
+    
+    if conn is not None:
+        conn.close()
