492862023f79a9cb9046a23b13b193dd9ce8f8bb
Breno Copeland <burenoaula@gmail.com>
2025-08-12 18:15:20 -0300

    listando elementos da tabela

--- settings.py
+++ settings.py
@@ -10,6 +10,8 @@ port = 5432
 conn = None
 cur = None
 
+rollback = []
+
 try:
 
     conn = psycopg2.connect(
@@ -24,31 +26,40 @@ try:
 
     cur = conn.cursor(cursor_factory= psycopg2.extras.DictCursor)
 
-
     while True:
 
         print("\nMenu:")
         print("1 - Inserir Usuario")
-        print("2 - Listar Usuario")
-        print("3 - Sair")
+        print("2 - Listar Usuarios")
+        print("3 - Confirmar Mudanças")
+        print("4 - Cancelar Mudanças")
+        print("5 - Sair")
 
         opcao = int(input("\nDigite a opção: ")) # Input único e claro
-
         
-        if opcao != 3:
+
+        if opcao != 5:
 
             if opcao == 1:
                 
-                name = input("Qual o nome do Usuário: ")
-                create_User(name)
-                #conn.commit()
+                name = input("\nQual o nome do Usuário: ")
+                create_User(cur, name)
             
             if opcao == 2:
 
-                name = input("Qual o nome que deseja buscar: ")
-                list_User(name)
-                #conn.commit()
-            
+                print("\n1 - Para visualizar a tabela completa")
+                print("2 - Para buscar por nome")
+
+                opcao = int(input("Selecione a opção de busca:"))
+
+                if opcao == 1:
+                    list_User(cur)
+
+                elif opcao == 2:
+                    nome = input("Qual o nome que deseja buscar: ")
+                    list_User(cur, nome)
+
+
         else:
                 
             print("Hasta la vista! \n")
