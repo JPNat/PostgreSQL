60639582b019ccac2eff9b1e231d73231f032995
Caiuã Gomes <69342553+Lentolan@users.noreply.github.com>
2025-08-14 15:08:13 -0300

    Att de pedrinho

--- settings.py
+++ settings.py
@@ -42,8 +42,14 @@ try:
 
             if opcao == 1:
                 
-                name = input("\nQual o nome do Usuário: ")
-                create_User(cur, name)
+                nome = input("\nQual o nome do Usuário: \n")
+                email = input("\nQual o email do Usuário: \n")
+                senha = input("\nQual a senha do Usuário: \n")
+                telefone = input("\nQual o telefone do Usuário: \n")
+                credito = 0
+                cargo_administrador = input("\nEsse usuário é Administrador?: Sim ou Não\n")
+
+                create_User(cur, conn, rollback, email, nome, telefone, credito, senha, cargo_administrador)
             
             if opcao == 2:
 
@@ -58,12 +64,23 @@ try:
                 elif opcao == 2:
                     nome = input("Qual o nome que deseja buscar: ")
                     list_User(cur, nome)
+            
+            if opcao == 3:
 
+                try:
+                    conn.commit()
 
-        else:
+                except Exception as error:
+                    print(error)
+
+            if opcao == 4:
+
+                rollback.pop()
+                print("Item removido de commit")
+
                 
-            print("Hasta la vista! \n")
-            break
+        print("Hasta la vista! \n")
+        break
 
     
 
