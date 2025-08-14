40164305f5a9eed8ba0a8ca44dbee21f326b86db
Caiuã Gomes <69342553+Lentolan@users.noreply.github.com>
2025-08-14 15:14:08 -0300

    Att Functions Write

--- functions.py
+++ functions.py
@@ -3,20 +3,20 @@ import psycopg2.extras
 
 #Aqui será onde teremos as funcionalidades
 
-def create_User(cur, roolback):
+def create_User(cur, conn, rollback, email, nome, telefone, credito, senha, cargo_administrador):
 
     try:
         insert_usuario = 'INSERT INTO usuario (email, nome, telefone, credito, senha, cargo_administrador) VALUES (%s, %s, %s, %f, %s, %s)'
         #with conn.cursor() as cursor:
         #cursor.execute(query, (id, name, email,))
         #conn.commit()
-        insert_value = ('fulaninho@gmail.com', 'Fulaninho Silva Santos', '79 9.1234-4321', 0.00, 'Senha Segura', 'Falso')
+        insert_value = (email, nome, telefone, credito, senha, cargo_administrador)
         cur.execute(insert_usuario)
 
         cur.execute('SELECT * FROM usuario')
         for record in cur.fetchall():
             print(record)
-            
+
         conn.commit()
 
         print('\n Criado com sucesso!')
