38ff622d5410bf90ef1aa5e58ad25f4ef545cb39
João Pedro <jpnatividade2016@gmail.com>
2025-08-14 16:55:27 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,13 +3,21 @@ import psycopg2.extras
 
 #Aqui será onde teremos as funcionalidades
 
-def create_User(cur, roolback):
+def create_User(cur, conn, rollback, email, nome, telefone, credito, senha, cargo_administrador):
 
     try:
-        novo_usuario = 'INSERT INTO usuario (email, nome, telefone, credito, senha, cargo_administrador) VALUES (%s, %s, %s, %f, %s, %s, %i)'
+        insert_usuario = 'INSERT INTO usuario (email, nome, telefone, credito, senha, cargo_administrador) VALUES (%s, %s, %s, %f, %s, %s)'
         #with conn.cursor() as cursor:
         #cursor.execute(query, (id, name, email,))
         #conn.commit()
+        insert_value = (email, nome, telefone, credito, senha, cargo_administrador)
+        cur.execute(insert_usuario)
+
+        cur.execute('SELECT * FROM usuario')
+        for record in cur.fetchall():
+            print(record)
+
+        conn.commit()
 
         print('\n Criado com sucesso!')
 
@@ -18,19 +26,16 @@ def create_User(cur, roolback):
 
 def list_User(cur, nome=''):
 
-    if nome == '':
-        cur.execute(f'SELECT * FROM usuario')
-    else:
-        cur.execute(f'SELECT * FROM usuario WHERE nome = \'{nome}\'')
 
     print()
 
     for item in cur.fetchall():
         print(item)
 
-
 def commit():
-    print()
+    print() 
+
+
 
 def rollback():
     print()
