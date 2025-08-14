cf29176b3cbc80cbea305a7eabd9cc212453da77
Caiuã Gomes <69342553+Lentolan@users.noreply.github.com>
2025-08-14 16:24:34 -0300

    Write?

--- write.py
+++ write.py
@@ -1,2 +1,18 @@
-def writeBD(X, field, value, log):
-    return X
+def writeBD(cur, conn, rollback, email, nome, telefone, credito, senha, cargo_administrador):
+    try:
+        insert_usuario = 'INSERT INTO usuario (email, nome, telefone, credito, senha, cargo_administrador) VALUES (%s, %s, %s, %f, %s, %s)'
+
+        insert_value = (email, nome, telefone, credito, senha, cargo_administrador)
+        
+        cur.execute(insert_usuario)
+
+        cur.execute('SELECT * FROM usuario')
+        for record in cur.fetchall():
+            print(record)
+
+        conn.commit()
+
+        print('\n Criado com sucesso!')
+
+    except:
+        print('error')
