42a921c00777d4ba7df6b91c4bb2e83679256b7a
JPNat <jpnatividade2016@gmail.com>
2025-08-14 16:23:27 -0300

    Salvando

--- settings.py
+++ settings.py
@@ -10,6 +10,8 @@ port = 5432
 conn = None
 cur = None
 
+rollback = []
+
 try:
 
     conn = psycopg2.connect(
@@ -24,35 +26,63 @@ try:
 
     cur = conn.cursor(cursor_factory= psycopg2.extras.DictCursor)
 
-
     while True:
 
         print("\nMenu:")
         print("1 - Inserir Usuario")
-        print("2 - Listar Usuario")
-        print("3 - Sair")
+        print("2 - Listar Usuarios")
+        print("3 - Confirmar Mudanças")
+        print("4 - Cancelar Mudanças")
+        print("5 - Sair")
 
         opcao = int(input("\nDigite a opção: ")) # Input único e claro
-
         
-        if opcao != 3:
+
+        if opcao != 5:
 
             if opcao == 1:
                 
-                name = input("Qual o nome do Usuário: ")
-                create_User(name)
-                #conn.commit()
+                nome = input("\nQual o nome do Usuário: \n")
+                email = input("\nQual o email do Usuário: \n")
+                senha = input("\nQual a senha do Usuário: \n")
+                telefone = input("\nQual o telefone do Usuário: \n")
+                credito = 0
+                cargo_administrador = input("\nEsse usuário é Administrador?: Sim ou Não\n")
+
+                rollback.append()
+
+                create_User(cur, conn, rollback, email, nome, telefone, credito, senha, cargo_administrador)
             
             if opcao == 2:
 
-                name = input("Qual o nome que deseja buscar: ")
-                list_User(name)
-                #conn.commit()
+                print("\n1 - Para visualizar a tabela completa")
+                print("2 - Para buscar por nome")
+
+                opcao = int(input("Selecione a opção de busca:"))
+
+                if opcao == 1:
+                    list_User(cur)
+
+                elif opcao == 2:
+                    nome = input("Qual o nome que deseja buscar: ")
+                    list_User(cur, nome)
             
-        else:
+            if opcao == 3:
+
+                try:
+                    conn.commit()
+
+                except Exception as error:
+                    print(error)
+
+            if opcao == 4:
+
+                rollback.pop()
+                print("Item removido de commit")
+
                 
-            print("Hasta la vista! \n")
-            break
+        print("Hasta la vista! \n")
+        break
 
     
 
